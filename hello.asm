a36d10469196d178741ab646363fe89e681f0c8f
Anton Sidorov <antosid@gmail.com>
2024-05-12 00:15:58 +0100

    refactor: optimize printing hello message and writing to file in hello.asm

--- hello.asm
+++ hello.asm
@@ -5,43 +5,23 @@ SYS_WRITE equ 1
 STDOUT equ 1
 _NOERRORS equ 0
 _ERRORS equ 1
-NL db 10
 
-%macro pushall 0
-    push rax
-    push rcx
-    push rdx
-    push rbx
-    push rbp
-    push rsi
-    push rdi
-%endmacro
-
-%macro popall 0
-    pop rdi
-    pop rsi
-    pop rbp
-    pop rbx
-    pop rdx
-    pop rcx
-    pop rax
-%endmacro
 section .text
 	global _start
 
 _start:
 	call open_file
-
 	call print_stars
-
 	call random_count
 	;mov rcx, 3
 
-_loop:
+.loop:
+	push rcx
 	call print_hello
 	call print_to_file
+	pop rcx
 	dec	rcx
-	jnz	_loop
+	jnz	.loop
 
 	call print_stars
 
@@ -51,27 +31,23 @@ _loop:
 	syscall
 
 print_hello:
-	pushall
-	mov	rdx, len_msg
-	mov	rsi, msg
 	mov	rdi, STDOUT
-	mov	rax, SYS_WRITE
-	syscall
-	popall
+	call print_msg
 	ret
 
 print_to_file:
-	pushall
+	mov rdi, [fd]
+	call print_msg
+	ret
+
+print_msg:
 	mov	rdx, len_msg
 	mov	rsi, msg
-	mov rdi, [fd]
 	mov	rax, SYS_WRITE
 	syscall
-	popall
 	ret
 
 open_file:
-	pushall
 	mov	rax, SYS_OPEN
 	mov rdi, filename
 	mov rsi, 1102o
@@ -81,42 +57,30 @@ open_file:
     je file_error
 
 	mov	[fd], rax
-	
-	popall
 	ret
 
 close_file:	
-	pushall
 	mov	rax, SYS_CLOSE
 	mov	rdi, [fd]
 	syscall
-	popall
 	ret
 
 print_stars:
-	pushall
 	mov	rdx, len_stars
 	mov	rsi, stars
 	mov	rdi, STDOUT
 	mov	rax, SYS_WRITE
 	syscall
-	mov	rdx, 1
-	mov	rsi, NL
-	mov	rdi, STDOUT
-	mov	rax, SYS_WRITE
-	syscall
-	popall
 	ret
 
 random_count:
-	rdrand rax
-	mov rcx , rax
-	shr rdx, 64
-	mov rcx, 10
-	idiv rcx
-	add rdx, 1
-	mov rcx, rdx
-	ret	
+    rdrand rax
+    mov rcx, 10
+    xor rdx, rdx
+    div rcx
+    add rdx, 1
+    mov rcx, rdx
+    ret
 
 file_error:
     mov rdi, _ERRORS
@@ -126,8 +90,9 @@ file_error:
 section .data
 	msg db 'Hello, world!', 0xa 
 	len_msg equ $ - msg
-	stars times len_msg  db '*', 0
-	len_stars equ $ - stars - 2
+	stars times len_msg - 1 db '*'
+	NL db 10
+	len_stars equ $ - stars
 	filename db 'file.txt', 0
 
 section .bss
